fe72c442b3f4f9746eb6233fea14eaaf9911da7d
rkkautsar <rkkautsar@gmail.com>
2015-09-22 00:23:56 +0700

    Simplify homepage

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ var pattern = Trianglify({
 	width: window.innerWidth + 200,
 	height: window.innerHeight + 200,
 	x_colors: 'random',
-	seed: 'rkkautsar/github/io',
-	cell_size: 50
+	seed: 'notSoRandom',
+	cell_size: 150
 });
 
 $(function() {
