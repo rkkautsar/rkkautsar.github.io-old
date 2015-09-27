ea90e324d86d2c4eca9a8c93816f7e6b4545f642
rkkautsar <rkkautsar@gmail.com>
2015-09-27 09:38:36 +0700

    Animate background

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
-var pattern = Trianglify({
-	width: window.innerWidth + 100,
-	height: window.innerHeight + 100,
-	x_colors: 'random',
-	seed: 'notSoRandom',
-	cell_size: 150
-});
+function changePattern() {
+	var d = new Date();
+	var pattern = Trianglify({
+		width: window.innerWidth + 100,
+		height: window.innerHeight + 100,
+		x_colors: 'Blues',
+		seed: d.getTime(),
+		cell_size: 150,
+		variance: 0.001
+	});
+	$('canvas').replaceWith(pattern.canvas());
+}
 
 $(function() {
-	$('header').append(pattern.canvas());
+	$('header').append($('<canvas></canvas>').attr('id', 'canvasPattern'));
+	setInterval(changePattern, 300);
 });
